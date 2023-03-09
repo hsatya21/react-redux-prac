849e78b639cf1a414e5ad2774b830d7dd50e2acb
Harshit Satya <hsatya@systango.com>
2023-03-09 11:26:21 +0530

    saga added

--- redux-saga-prac/src/redux/productSaga.js
+++ redux-saga-prac/src/redux/productSaga.js
@@ -1,5 +1,5 @@
 import { put, takeEvery } from "redux-saga/effects";
-import { ADD_TO_CART, PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
 
 function* getProducts() {
   let data = yield fetch("http://localhost:3005/product");
