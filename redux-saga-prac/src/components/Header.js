6489cfc138e7bd0c611552243151a3dddfb80d24
Harshit Satya <hsatya@systango.com>
2023-03-09 15:17:06 +0530

    search feature added

--- redux-saga-prac/src/components/Header.js
+++ redux-saga-prac/src/components/Header.js
@@ -1,16 +1,30 @@
 import "../App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { productSearch } from "../redux/productAction";
 // import { cartData } from "../redux/reducer";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const result = useSelector((state) => state.cartData);
+
+  const handleSearch = (event) => {
+    dispatch(productSearch(event.target.value));
+  };
+
   console.log("Redux data in header:", result);
   return (
     <div className="header">
       <Link to="/">
         <h1 className="logo">E-Comm-Store</h1>
       </Link>
+      <div className="search-box">
+        <input
+          type="text"
+          onChange={handleSearch}
+          placeholder="Search Product"
+        />
+      </div>
       <Link to="/cart">
         <div className="cart-div">
           <span>{result.length}</span>
--- redux-saga-prac/src/redux/productSaga.js
+++ redux-saga-prac/src/redux/productSaga.js
@@ -1,8 +1,8 @@
 import { put, takeEvery } from "redux-saga/effects";
-import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
+import { PRODUCT_LIST, SET_PRODUCT_LIST, PRODUCT_SEARCH } from "./constant";
 
 function* getProducts() {
-  let data = yield fetch("http://localhost:3005/product");
+  let data = yield fetch("http://localhost:3005/products");
   data = yield data.json();
   yield put({
     type: SET_PRODUCT_LIST,
@@ -10,8 +10,19 @@ function* getProducts() {
   });
 }
 
+function* searchProducts(data) {
+  let result = yield fetch(`http://localhost:3005/products?q=${data.query}`);
+  result = yield result.json();
+  // console.log("action search", data);
+  yield put({
+    type: SET_PRODUCT_LIST,
+    data: result,
+  });
+}
+
 function* productSaga() {
   yield takeEvery(PRODUCT_LIST, getProducts);
+  yield takeEvery(PRODUCT_SEARCH, searchProducts);
 }
 
 export default productSaga;
